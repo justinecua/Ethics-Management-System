a51d3413fa1a05e6076aed8bd33b546c5e869a4e
james121i <e.maglangit221i@gmail.com>
2024-10-02 17:42:27 +0800

    UI update Calendar Dashboard

--- EthicsProject/EthicsApp/views/views.py
+++ EthicsProject/EthicsApp/views/views.py
@@ -3,17 +3,18 @@ from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.urls import reverse
 from .models import Schedule
 from datetime import datetime
+from django.contrib import messages 
 
 def index(request):
     return render(request, 'LandingPage.html')
 
 def schedule_view(request):
     schedules = Schedule.objects.all()  
-    return render(request, 'CalendarSchedule.html', {'schedules': schedules}) 
+    return render(request, 'Reviewer/CalendarSchedule.html', {'schedules': schedules}) 
 
 def schedule_list(request):
     schedules = Schedule.objects.all()
-    return render(request, 'ScheduleList.html', {'schedules': schedules})
+    return render(request, 'Reviewer/ScheduleList.html', {'schedules': schedules})
 
 def AddSchedule(request):
     if request.method == 'POST':
@@ -25,13 +26,28 @@ def AddSchedule(request):
             datetime_obj = datetime.strptime(datetime_str, '%Y-%m-%dT%H:%M')
             schedule = Schedule(schedname=schedname, datetime=datetime_obj, description=description)
             schedule.save()
-
-            return JsonResponse({'success': True})  # Respond with success
+            
+            return JsonResponse({'success': True}) 
         else:
             return JsonResponse({'success': False, 'error': 'Invalid date/time'}, status=400)
 
     # Handle GET request if necessary
-    return render(request, 'AddSchedule.html')
+    return render(request, 'Reviewer/AddSchedule.html')
+
+def AddSchedule2(request):
+    if request.method == 'POST':
+        schedname = request.POST.get('schedname', "Unnamed Schedule")
+        datetime_str = request.POST.get('datetime')
+        description = request.POST.get('description', '')
+
+        if datetime_str:
+            datetime_obj = datetime.strptime(datetime_str, '%Y-%m-%dT%H:%M')
+            schedule = Schedule(schedname=schedname, datetime=datetime_obj, description=description)
+            schedule.save()
+            
+        messages.success(request, 'Schedule added successfully!')
+            
+    return render(request, 'Reviewer/AddSchedule.html')
 
 
 def update_schedule(request, schedule_id):
@@ -48,9 +64,13 @@ def update_schedule(request, schedule_id):
         schedule.save()
         return HttpResponseRedirect(reverse('schedule_list'))
     
-    return render(request, 'UpdateSchedule.html', {'schedule': schedule})
+    return render(request, 'Reviewer/UpdateSchedule.html', {'schedule': schedule})
 
 def delete_schedule(request, schedule_id):
     schedule = get_object_or_404(Schedule, id=schedule_id)
     schedule.delete()
     return HttpResponseRedirect(reverse('schedule_list'))
+
+def dashboard (request):
+    schedules = Schedule.objects.all()
+    return render (request, 'Reviewer/dashboard.html', {'schedules':schedules})
