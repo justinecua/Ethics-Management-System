96b3daa731864e1448bb09735118b9b45e9308c1
justinecua <katsukiyu38@gmail.com>
2024-10-02 15:15:34 +0800

    configured settings.py for production2

--- EthicsProject/EthicsProject/settings.py
+++ EthicsProject/EthicsProject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['smcethicsoffice.live']
+ALLOWED_HOSTS = ['smcethicsoffice.live', '127.0.0.1:8000', '127.0.0.1', '68.183.181.232']
 
 
 # Application definition
