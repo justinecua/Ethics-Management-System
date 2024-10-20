7bf182bb1eba6e350fa7d3041e34f97a83304226
justinecua <katsukiyu38@gmail.com>
2024-10-20 19:27:03 +0800

    update test for merge

--- EthicsProject/EthicsApp/views/views.py
+++ EthicsProject/EthicsApp/views/views.py
@@ -12,6 +12,7 @@ from django.core.mail import send_mail, EmailMultiAlternatives
 from django.conf import settings
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
+from django.contrib.auth import login
 
 
 def index(request):
--- EthicsProject/EthicsProject/settings.py
+++ EthicsProject/EthicsProject/settings.py
@@ -122,14 +122,12 @@ SOCIALACCOUNT_PROVIDERS = {
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
+
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'OPTIONS': {
+            'min_length': 6,
+        }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
