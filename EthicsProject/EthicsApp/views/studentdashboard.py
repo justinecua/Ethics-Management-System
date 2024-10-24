749a94e64c9d99e206c1a2f1ea2e1c979e5c129a
james121i <e.maglangit221i@gmail.com>
2024-10-24 10:13:02 +0800

    update studentnav

--- EthicsProject/EthicsApp/views/studentdashboard.py
+++ EthicsProject/EthicsApp/views/studentdashboard.py
@@ -1,8 +1,9 @@
 from django.template import loader
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-
+@login_required(login_url='login')
 def studentdashboard(request):
   template = loader.get_template('students/studentdashboard.html')
   return HttpResponse(template.render())
