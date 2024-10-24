234f6b5278dc18f3f639a2c1e13a9c1c33cb1f39
justinecua <katsukiyu38@gmail.com>
2024-10-24 12:09:34 +0800

    fix error of url after merging

--- EthicsProject/EthicsApp/urls.py
+++ EthicsProject/EthicsApp/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     #-----------------------------------------Justine------------------------------------------------
     re_path(r'^accounts/login/$', views.login2, name='login2'),
     re_path(r'^accounts/signup/$', views.signup, name='signup'),
-    re_path(r'^adminDashboard/$', views.adminDashboard, name='adminDashboard'), 
+    re_path(r'^adminDashboard/$', views.adminDashboard, name='adminDashboard'),
     re_path(r'^adminAccounts/$', views.adminAccounts, name='adminAccounts'),
     re_path(r'^adminAppointments/$', views.adminAppointments, name='adminAppointments'),
     re_path(r'^adminManuscripts/$', views.adminManuscripts, name='adminManuscripts'),
@@ -44,7 +44,7 @@ urlpatterns = [
   re_path(r'^studentAppointment/$', views.studentAppointment, name='studentAppointment'),
   re_path(r'^studentManuscript/$', views.studentManuscript, name='studentManuscript'),
    re_path(r'^studentSettings/$', views.studentSettings, name='studentSettings'),
-  
+
 
 
 
@@ -78,18 +78,18 @@ urlpatterns = [
 
     #------------------------------------------Erwin------------------------------------------------
 
-    path('Reviewer/calendar/', views.schedule_view, name='schedule_view'),  
-    path('Reviewer/calendar/schedule/', views.AddSchedule, name='AddSchedule'), 
-    path('Reviewer/calendar/schedule2/', views.AddSchedule2, name='AddSchedule2'), 
-    path('Reviewer/calendar/schedule-list/', views.schedule_list, name='schedule_list'),  
-    path('schedules/update/<int:schedule_id>/', views.update_schedule, name='update_schedule'),  
+    path('Reviewer/calendar/', views.schedule_view, name='schedule_view'),
+    path('Reviewer/calendar/schedule/', views.AddSchedule, name='AddSchedule'),
+    path('Reviewer/calendar/schedule2/', views.AddSchedule2, name='AddSchedule2'),
+    path('Reviewer/calendar/schedule-list/', views.schedule_list, name='schedule_list'),
+    path('schedules/update/<int:schedule_id>/', views.update_schedule, name='update_schedule'),
     path('schedules/delete/<int:schedule_id>/', views.delete_schedule, name='delete_schedule'),
     path('Reviewer/dashboard/', views.dashboard, name='schedule_dashboard'),
 
 
     path('schedule_list/', views.schedule_list, name='schedule_list'),
 
-    path('student/appointment/', views.student_appointment, name='student_appointment'),
+
 
 
 
